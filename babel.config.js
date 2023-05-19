a2fd19cdd30ed2108d0f9840dd0f39beac9ab16d
Hans <admin@itsrv.tw>
2023-05-19 13:34:58 +0800

    Fix error

--- babel.config.js
+++ babel.config.js
@@ -2,11 +2,16 @@ module.exports = {
   presets: ['module:metro-react-native-babel-preset'],
   plugins: [
     '@babel/plugin-proposal-export-namespace-from',
+    'babel-plugin-transform-import-meta',
     ['module-resolver', {
       alias: {
+        'stream/web': 'stream-browserify',
         'stream': 'stream-browserify',
         'buffer': '@craftzdog/react-native-buffer',
         'zlib': 'browserify-zlib',
+        'fs': 'react-native-fs',
+        'path': 'path-browserify',
+        'onnxruntime-node': 'onnxruntime-react-native',
       },
     }],
   ],
