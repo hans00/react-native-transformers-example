3023eb679b19a2c3c80798a907195f6a0d98100b
Hans <hans.chen@bricks.tools>
2025-07-06 03:10:45 +0800

    Remove unused

--- babel.config.js
+++ babel.config.js
@@ -1,7 +1,6 @@
 module.exports = {
   presets: ['module:@react-native/babel-preset'],
   plugins: [
-    '@babel/plugin-proposal-export-namespace-from',
     ['module-resolver', {
       alias: {
         'buffer': '@craftzdog/react-native-buffer',
